256583ff5202e0cd53b85832311e5d127790b29b
W.A.Punsisi Yemani Perera <wapunsisiyemani18@gmail.com>
2025-02-25 22:53:59 +0530

    hi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,13 +14,10 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
+            Hi Yemani{" "}
+            
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Just say Hi.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
@@ -37,7 +34,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            GO now
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
